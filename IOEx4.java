f7d4fdc994c44cfe2053727424f30c4a61d56d02
syjo00 <81938190+syjo00@users.noreply.github.com>
2023-01-05 23:31:45 +0900

    Add files via upload

---
+++ IOEx4.java
@@ -0,0 +1,52 @@
+package IO;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class IOEx4 {
+
+	public static void main(String[] args) {
+		
+		
+		byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
+		byte[] outSrc = null;
+		byte[] temp = new byte[4];
+		
+		
+		ByteArrayInputStream input = null;
+		ByteArrayOutputStream output  = null;
+		
+		
+		input = new ByteArrayInputStream(inSrc);
+		output = new ByteArrayOutputStream();
+	
+			try {
+				
+				while(input.available()>0) {
+					int len = input.read(temp); //읽어온 데이터의 개수를 반환한다.
+					output.write(temp,0,len); //읽어온 만큼만 write한다. --temp의 0~len(숫자)자리까지의 문자를 output 에 담는다.
+					
+				}
+				
+			}catch(IOException e){
+	
+		}
+			
+			outSrc = output.toByteArray(); //스트림의 내용을 byte 배열로 반환한다.
+			
+			System.out.println("Input Source :" + Arrays.toString(inSrc));
+			System.out.println("Output Source :"+Arrays.toString(outSrc));
+			System.out.println("temp :"+Arrays.toString(temp));
+			
+			/*
+				Input Source :[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+				Output Source :[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+				temp :[8, 9, 6, 7]
+			*/
+	
+	}
+	
+}
