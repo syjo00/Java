9c74655ea5827b45ddabc9de6a6069ee416473e1
syjo00 <81938190+syjo00@users.noreply.github.com>
2024-07-10 22:09:32 +0900

    Add files via upload

---
+++ P1874_스택수열.java
@@ -0,0 +1,62 @@
+package algorithm;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P1874_스택수열 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);		
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		StringBuffer bf = new StringBuffer();
+		
+		int num = 1;//오름차순 수
+		
+		boolean result = true;
+		
+		for(int i=0;i<A.length;i++) {
+			
+			int su = A[i];	//현재 수열의 수
+			
+			if(su>=num) {	//현재 수열 값>=오름차순 자연수 : 값이 같아질 때까지 push()수행
+				
+				while(su>=num) {//push()
+					stack.push(num++);
+					bf.append("+_\n");
+				}
+				
+				stack.pop();
+				bf.append("-/n");
+						
+			}else {//현재 수열 값<오름차순 자연수 : pop()을 수행 해 수열 원소를 꺼냄
+				
+				int n = stack.pop();
+				//스택의 가장 위의수가 만들어야 하는 수열의 수보다 크면 수열을 출력할 수 없음
+				if(n>su) {
+					System.out.println("NO");
+					result = false;
+					break;
+					
+				}else {
+					bf.append("-\n");					
+				}
+				
+			}
+		
+		}
+		
+		if(result)
+			System.out.println(bf.toString());
+		
+	}//main
+
+}//class
