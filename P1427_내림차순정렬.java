2eeba4003f3c24faf3fa705d0ca509a60a148767
syjo00 <81938190+syjo00@users.noreply.github.com>
2024-08-11 17:20:10 +0900

    Add files via upload

---
+++ P1427_내림차순정렬.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class P1427_내림차순정렬 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int[] A = new int[str.length()];
+		
+		for(int i=0;i<str.length();i++) {
+		
+			A[i] = Integer.parseInt(str.substring(i, i+1));
+				
+			int Max = i;
+			
+			for(int j=i+1;j<str.length();j++) {
+				
+				if(A[j]>A[Max]) {
+					Max = j;
+				}
+				
+				
+				if(A[i]<A[Max]) {
+					int temp = A[i];
+					A[i] = A[Max];
+					A[Max] = temp;
+				}
+								
+			}//for1
+		
+			
+			for(int x =0; x<str.length();x++) {
+				System.out.print(A[x]);
+			}
+			
+		}//for2		
+				
+	}//main
+	
+	
+}//class
